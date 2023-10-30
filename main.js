bef5e50cc6ef7ee1bf1ea25b77654a82eb33213e
Brandyn Coverdill <brandyncoverdill98@gmail.com>
2023-10-30 00:07:35 -0600

    Feature: Added grayscale / rainbow option
    Added buttons on the top of the screen so the user can switch
    between grayscale and rainbow on the same project.

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const body = document.querySelector('body');
 const header = document.querySelector('header');
 let height = 16;
 let width = 16;
+let color = 'rainbow';
 
 // Create a container for the grid of specified width/height
 let gridContainer = createGrid(height, width);
@@ -53,7 +54,7 @@ function createGrid(row = 16, column = 16) {
  */
 function formatGridItems() {
 	const gridItems = document
-		.querySelector('body div')
+		.querySelector('body > div')
 		.getElementsByTagName('div');
 
 	for (let i = 0; i < gridItems.length; i++) {
@@ -62,30 +63,93 @@ function formatGridItems() {
 			opacity: 1;
 		`;
 
+		// Remove background: #${Math.floor... to have it grayscale}
 		gridItems[i].addEventListener('mouseenter', () => {
-			gridItems[i].style.cssText += `
-				background: #${Math.floor(Math.random() * 16777215).toString(16)};
-			`;
-			gridItems[i].style.opacity -= 0.1;
+			// If rainbow is selected, run the following line so the grid is rainbow
+			switch (color) {
+				case 'rainbow':
+					gridItems[i].style.cssText += `
+						background: #${Math.floor(Math.random() * 16777215).toString(16)};
+					`;
+					gridItems[i].style.opacity -= 0.1;
+					break;
+
+				case 'grayscale':
+					gridItems[i].style.opacity -= 0.1;
+					break;
+
+				default:
+					console.log('error in switch statement for mouseenter event...');
+					break;
+			}
 		});
 	}
 }
 
-// Create button and append to the top of the screen
-const button = document.createElement('button');
-button.textContent = 'Change grid size';
+// Create button and append to the top of the screen for changing grid size
+const changeGridSizeBtn = document.createElement('button');
+changeGridSizeBtn.textContent = 'Change grid size';
+changeGridSizeBtn.style.cssText = `
+	padding: .5em 1em;
+	font-weight: bold;
+	cursor: pointer;
+`;
+header.appendChild(changeGridSizeBtn);
 
-// Style button
-button.style.cssText = `
+// Add div with two buttons where the user can choose grayscale or rainbow
+const colorOptionsDiv = document.createElement('div');
+const grayscaleBtn = document.createElement('button');
+const rainbowBtn = document.createElement('button');
+
+grayscaleBtn.textContent = 'Change to Rainbow';
+rainbowBtn.textContent = 'Change to Grayscale';
+
+colorOptionsDiv.style.cssText = `
+	display: flex;
+	flex-wrap: wrap;
+	gap: 1em;
+	justify-content: center;
+	margin-block-start: 1em;
+`;
+
+grayscaleBtn.style.cssText = `
 	padding: .5em 1em;
 	font-weight: bold;
 	cursor: pointer;
 `;
 
-header.appendChild(button);
+rainbowBtn.style.cssText = `
+	padding: .5em 1em;
+	font-weight: bold;
+	cursor: pointer;
+`;
+
+header.appendChild(colorOptionsDiv);
+colorOptionsDiv.appendChild(grayscaleBtn);
+colorOptionsDiv.appendChild(rainbowBtn);
+
+// On refresh, disable grayscale and enable rainbow
+window.onload = function () {
+	rainbowBtn.removeAttribute('disabled');
+	grayscaleBtn.setAttribute('disabled', true);
+	color = 'rainbow';
+};
+
+// @todo: add events for button to disable the other button
+rainbowBtn.addEventListener('click', () => {
+	color = 'grayscale';
+	rainbowBtn.toggleAttribute('disabled');
+	grayscaleBtn.toggleAttribute('disabled');
+});
+
+grayscaleBtn.addEventListener('click', () => {
+	color = 'rainbow';
+	rainbowBtn.toggleAttribute('disabled');
+	grayscaleBtn.toggleAttribute('disabled');
+});
 
 // Grab user input so the grid can be the size the user wants
-button.addEventListener('click', () => {
+changeGridSizeBtn.addEventListener('click', () => {
 	let temp = prompt(
 		'Enter a number of squares per side you want the grid to be.'
 	);
@@ -96,13 +160,13 @@ button.addEventListener('click', () => {
 		alert('Please enter a valid number less than or equal to 100.');
 		return;
 	}
-
-	// Remove the old grid and replace with the new grid
 	height = temp;
 	width = temp;
+
+	// Remove the old grid and replace with the new grid
 	body.removeChild(gridContainer);
 	gridContainer = createGrid(height, width);
-	body.appendChild(gridContainer);
 	formatGrid(gridContainer);
+	body.appendChild(gridContainer);
 	formatGridItems();
 });
